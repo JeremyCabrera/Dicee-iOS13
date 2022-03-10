d688232b4d7b0e3753e52439eca1f90f7366148b
Jeremy Cabrera <jeremy.cabrera@outlook.com>
2022-03-10 16:10:17 -0500

    added function to randomize rolled dice output

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,19 +13,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-    }
-
+    var leftDiceNumber = 1
+    var rightDiceNumber = 1
+    
+   
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
-    
-    }
-    
+        
+       
+        print("leftDiceNumber at beginning = \(leftDiceNumber)")
+        diceImageView1.image = [  #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [leftDiceNumber]
+        
+        leftDiceNumber = leftDiceNumber + 1
+       
+        print("leftDiceNumber at the end = \(leftDiceNumber)")
+        
+       
+        print("rightDiceNumber at beginning = \(rightDiceNumber)")
+        diceImageView2.image = [  #imageLiteral(resourceName: "DiceSix"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceOne")] [rightDiceNumber]
+        
+        
+        rightDiceNumber = rightDiceNumber + 1
+       
+        print("rightDiceNumber at the end = \(rightDiceNumber)")
+        
+        
+        
 }
-
+}
